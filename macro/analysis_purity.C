5c6bfb29a96c2a0583b68f8d9a9133fd485e4c90
christopher dilks <christopher.j.dilks@gmail.com>
2021-10-27 17:57:50 -0400

    update crossing angles in all macros

--- macro/analysis_purity.C
+++ macro/analysis_purity.C
@@ -5,7 +5,7 @@ void analysis_purity(
     TString infiles="datarec/in.config", /* delphes tree(s) */
     Double_t eleBeamEn=5, /* electron beam energy [GeV] */
     Double_t ionBeamEn=41, /* ion beam energy [GeV] */
-    Double_t crossingAngle=0, /* crossing angle [mrad] */
+    Double_t crossingAngle=25, /* crossing angle [mrad] */
     TString methodname="ele", /*reconstruction method name*/
     TString outfilePrefix="resolutions" /* output filename prefix*/
     
