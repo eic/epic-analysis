c0e4e11420745df93fa8944bada67cbad9821c2c
Christopher Dilks <c-dilks@users.noreply.github.com>
2021-11-15 23:29:57 -0500

    generalize CI macros for fast or full sim (#88)
    * combine fastsim and fullsim coverage CI test
    
    * add build artifacts
    
    * attempt job matrix
    
    * modified:   ../../.github/workflows/ci.yml
    
    * add fastsim macros to matrix
    
    * add mode to matrix; generalize other macros
    
    * probably wrong
    
    * probably wrong
    
    * hopefully not wrong
    
    * artifacts collection
    
    * add config file to matrix
    
    * modified:   .github/workflows/ci.yml
    
    * merge fastsim and fullsim artifacts to same dir
    
    * modified:   .github/workflows/ci.yml
    
    * consolidation and extension
    
    * modified:   .github/workflows/ci.yml
    
    * modified:   macro/ci/postprocess_xq.C
    
    * modified:   macro/ci/postprocess_xq.C and cull step
    
    * use HistConfig log settings in DrawInBins
    
    * make labels legible

---
+++ macro/ci/organize-artifacts.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+# organize artifacts by combining fast and full sim directories
+# - adds `.fastsim` or `.fullsim` suffixes to filenames
+# - execute in CI pipelines from top-level directory, after all artifacts
+#   have been collected into one directory, specified by argument
+
+# arguments
+if [ $# -ne 1 ]; then echo "USAGE: $0 [artifacts-dir]"; exit 2; fi
+pushd $1
+
+# loop through *fastsim* directories
+ls | grep fastsim |\
+while read dirFast; do
+
+  # set fullsim and output directory names
+  echo "--------------------"
+  dirFull=$(echo $dirFast | sed 's/fastsim/fullsim/g')
+  dirOut=$(echo $dirFast | sed 's/fastsim//g' | sed 's/--/-/g')
+  mkdir -p $dirOut
+
+  # move fastsim artifacts to output directory, and add suffix to file name
+  echo "MOVE AND RENAME artifacts in $dirFast -> $dirOut"
+  pushd $dirFast
+  for file in *; do
+    mv -v $file ../$dirOut/$(echo $file | sed 's/^.*\./&fastsim./g')
+  done
+  popd
+
+  # repeat for fullsim artifacts
+  echo "MOVE AND RENAME artifacts in $dirFull -> $dirOut"
+  pushd $dirFull
+  for file in *; do
+    mv -v $file ../$dirOut/$(echo $file | sed 's/^.*\./&fullsim./g')
+  done
+  popd
+
+  # remove empty directories
+  echo "CLEANUP"
+  rm -rv $dirFast $dirFull
+
+done
+popd
