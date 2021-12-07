289e839249e1c573f6633ba5f7c777f33e530f6d
christopher dilks <christopher.j.dilks@gmail.com>
2021-12-07 03:37:51 -0500

    modified:   macro/ci/localtest.sh

--- macro/ci/localtest.sh
+++ macro/ci/localtest.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 # sandbox for local tests and development; not executed by CI
 
-# reset
+### reset
 # rm -rv out/*; touch out/.keep
 
-# beam energy
-e=10
-p=100
+### beam energy
+# e=10; p=100
+e=18; p=275
 
-# high stats comparisons
-fastconfig="datarec/arc/fastS3/10x100/delphes.config"
-fullconfig="datarec/deathvalley-v1.0/10x100/files.config"
+### high stats comparisons
+fastconfig="datarec/arc/fastS3/${e}x${p}/delphes.config"
+fullconfig="datarec/deathvalley-v1.0/${e}x${p}/files.config"
 
-# loop over recon methods
+### loop over recon methods
 for recon in ele jb da; do
+# for recon in ele; do
   root -b -q 'macro/ci/analysis_x_q2.C("'$fastconfig'",'$e','$p',-25,"xq.'$recon'.fastsim","'$recon'")' &
   root -b -q 'macro/ci/analysis_x_q2.C("'$fullconfig'",'$e','$p',-25,"xq.'$recon'.fullsim","'$recon'")' &
   wait
