9246d49f23d544ba4253e94b4efd2bd9223ea32d
Gregtom3 <gamatousek@gmail.com>
2022-07-26 13:40:54 -0400

    Module load

--- container/shell.sh
+++ container/shell.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 # start an interactive shell in the singularity container
-
+module load singularity
 # determine top-level directory
 if [ -z "${BASH_SOURCE[0]}" ]; then topDir=$(dirname $(realpath $0))/..
 else topDir=$(dirname $(realpath ${BASH_SOURCE[0]}))/..; fi
 
 # set image file
 imgDir=$topDir/container/img
-imgFile=$imgDir/sidis-eic.sif
+imgFile=$imgDir/largex-eic.sif
 
 # start shell
 args=$@
