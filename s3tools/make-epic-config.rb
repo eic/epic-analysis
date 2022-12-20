67066178d04c4c212645198e6b7e9b174cf0f392
Christopher Dilks <christopher.j.dilks@gmail.com>
2022-12-20 14:03:48 -0500

    ci: use updated s3tools/make-epic-config.rb

--- s3tools/make-epic-config.rb
+++ s3tools/make-epic-config.rb
@@ -5,26 +5,39 @@
 
 require 'optparse'
 require 'ostruct'
-# require 'awesome_print'
+require 'fileutils'
 
 # default CLI options
 options = OpenStruct.new
-options.version    = '22.11.3'
+options.version    = 'epic.22.11.3'
 options.energy     = '18x275'
-options.locDir     = "epic.#{options.version}"
-options.mode       = 'd'
-options.limit      = 5
+options.locDir     = ''
+options.mode       = 's'
+options.limit      = 2
 options.configFile = ''
 options.detector   = 'arches'
 options.radCor     = false
 
+# global settings
+CrossSectionTable = 'datarec/xsec/xsec.dat'
+HostURL           = 'https://eics3.sdcc.bnl.gov:9000'
+
+
 # production specifications, latest first
+# PRODUCTION_VERSION => {
+#   :comment         => Description about this version
+#   :crossSectionID  => Proc(minQ2,maxQ2,radDir) -> label of row of `CrossSectionTable`
+#   :releaseSubDir   => Proc() -> Directory for this PRODUCTION_VERSION
+#   :energySubDir    => Proc() -> Subdirectory associated to user-specified beam energy
+#   :dataSubDir      => Proc(*version dependent*) -> Subdirectory of `:energySubDir`
+# }
 prodSettings = {
-  '22.11.3' => {
-    :comment    => 'Pythia 6, with & without radiative corrections',
-    :releaseDir => Proc.new { "S3/eictest/EPIC/RECO/#{options.version}/epic_#{options.detector}/SIDIS/pythia6" },
-    :energyDir  => Proc.new { "ep_#{options.energy}" },
-    :dataDir    => Proc.new { |radDir|
+  'epic.22.11.3' => {
+    :comment         => 'Pythia 6, with & without radiative corrections',
+    :crossSectionID  => Proc.new { |minQ2,maxQ2,radDir| "pythia6:ep_#{radDir}.#{options.energy}_q2_#{minQ2}_#{maxQ2}" },
+    :releaseSubDir   => Proc.new { "S3/eictest/EPIC/RECO/#{options.version.sub('epic.','')}/epic_#{options.detector}/SIDIS/pythia6" },
+    :energySubDir    => Proc.new { "ep_#{options.energy}" },
+    :dataSubDir      => Proc.new { |radDir|
       if [options.energy,radDir]==['18x275','noradcor']  # correct for S3 disorganization
         "hepmc_ip6"
       else
@@ -32,11 +45,12 @@ prodSettings = {
       end
     },
   },
-  '22.11.2' => {
-    :comment    => 'Pythia 8',
-    :releaseDir => Proc.new { "S3/eictest/EPIC/RECO/#{options.version}/epic_#{options.detector}/DIS/NC" },
-    :energyDir  => Proc.new { "#{options.energy}" },
-    :dataDir    => Proc.new { |minQ2| "minQ2=#{minQ2}" },
+  'epic.22.11.2' => {
+    :comment         => 'Pythia 8',
+    :crossSectionID  => Proc.new { |minQ2| "pythia8:#{options.energy}/minQ2=#{minQ2}" },
+    :releaseSubDir   => Proc.new { "S3/eictest/EPIC/RECO/#{options.version.sub('epic.','')}/epic_#{options.detector}/DIS/NC" },
+    :energySubDir    => Proc.new { "#{options.energy}" },
+    :dataSubDir      => Proc.new { |minQ2| "minQ2=#{minQ2}" },
   },
 }
 
@@ -81,7 +95,7 @@ OptionParser.new do |o|
   o.separator ''
   o.on("-o", "--output [OUTPUT_DIRECTORY]",
        "Local output 'datarec/' subdirectory name",
-       "Default: #{options.locDir} (-> datarec/#{options.locDir})"
+       "Default: [PRODUCTION_VERSION] (-> datarec/[PRODUCTION_VERSION])"
       ) do |a|
         options.locDir = a
       end
@@ -90,7 +104,7 @@ OptionParser.new do |o|
        "specify what to do:",
        "  s - make config file for streaming from S3",
        "  d - download from S3, then make the local config file",
-       "  c - just make the local config file, for local files",
+       "  c - just make the local config file, for downloaded local files",
        "Default: #{options.mode}"
       ) do |a|
         unless ['s','d','c'].include? a
@@ -146,6 +160,17 @@ OptionParser.new do |o|
 end.parse!( ARGV.length>0 ? ARGV : ['--help'] )
 puts "OPTIONS: #{options}"
 
+# get release and energy subdirectories, for the user-specified release version
+prod = prodSettings[options.version]
+prod[:releaseDir] = prod[:releaseSubDir].call
+prod[:energyDir]  = prod[:releaseDir] + '/' + prod[:energySubDir].call
+puts "Release Dir: #{prod[:releaseDir]}"
+puts "Energy Dir:  #{prod[:energyDir]}"
+# system "mc tree #{prod[:releaseDir]}"
+
+# set target `locDir` directory
+prod[:targetDir] = "datarec/#{options.locDir.empty? ? options.version : options.locDir}"
+
 
 ## helper functions
 # get a list of files on S3 at `dir`
@@ -154,92 +179,142 @@ def mc_ls(dir)
     .split(/\n/)
     .map{ |line| line.split.last }
 end
+# get the cross section from `CrossSectionTable`
+getCrossSection = Proc.new do |searchPattern|
+  rows = File.readlines(CrossSectionTable).grep(/^#{searchPattern} /)
+  if rows.size == 1
+    rows.first.split[1].to_f
+  else
+    $stderr.puts "ERROR: missing cross section for search pattern '#{searchPattern}'"    if rows.size == 0
+    $stderr.puts "ERROR: duplicated cross section for search pattern '#{searchPattern}'" if rows.size > 1
+    0.0
+  end
+end
 
 
-# get energy subdirectory
-prod = prodSettings[options.version]
-releaseDir = prod[:releaseDir].call
-energyDir = releaseDir + '/' + prod[:energyDir].call
-puts "Release Dir: #{releaseDir}"
-puts "Energy Dir:  #{energyDir}"
-# system "mc tree #{releaseDir}"
+
+# RELEASE VERSION DEPENDENT CODDE ##################
+# Fill the following additional elements of `prod`:
+# -> prod[:q2ranges]  list of pairs [minQ2,maxQ2] for each Q2 range
+# -> prod[:dataDirs]  list of data directories for each Q2 range
+# -> prod[:fileLists] list of files for each Q2 range
+# -> prod[:radDir] the name of the radiative corrections directory (if applicable)
+####################################################
+
+if ['epic.22.11.3'].include? options.version
+  # set source data directory
+  prod[:radDir] = options.radCor ? 'radcor' : 'noradcor'
+  dataDir = prod[:energyDir] + '/' + prod[:dataSubDir].call(prod[:radDir])
+  puts "Data Dir: #{dataDir}"
+  # set target directory
+  prod[:targetDir] += "/"+prod[:radDir]
+  puts "Target Dir: #{prod[:targetDir]}"
+  # check if there are any files
+  if mc_ls("#{dataDir} | head").empty?
+    $stderr.puts "ERROR: no files in this Data Dir"
+    puts "Available Data Directory Tree (be patient...)"
+    system "mc tree #{prod[:releaseDir]}"
+    exit 1
+  end
+  # get the Q2 ranges
+  puts "Getting the full list of files... be patient..."
+  fullList = mc_ls(dataDir).grep(/\.root$/)
+  prod[:q2ranges] = fullList
+    .map{ |file| file.gsub(/.*_q2_/,'').sub(/_run.*/,'') }
+    .uniq
+    .map{ |range| range.split('_').map &:to_i }
+  puts "Q2 ranges: #{prod[:q2ranges]}"
+  # set dataDirs (the same for each Q2 range)
+  prod[:dataDirs] = prod[:q2ranges].map{ |q2range| dataDir }
+  # get a list of files for each Q2 range
+  puts "File names for each Q2 range:"
+  prod[:fileLists] = prod[:q2ranges].map do |minQ2, maxQ2|
+    fileList = fullList
+      .grep(/q2_#{minQ2}_#{maxQ2}/)
+      .first(options.limit)
+    puts "--- #{minQ2} < Q2 < #{maxQ2}"
+    fileList.each{ |file| puts "  #{file}" }
+    fileList
+  end
 
 
-# VERSION DEPENDENT SPAGHETTI ##############
-# If downloading or streaming, get the Q2 ranges and file lists from S3
-# -> prod[:q2ranges]  list of pairs [minQ2,maxQ2]
-# -> prod[:fileLists] list of files for each Q2 range in prod[:q2ranges]
-############################################
-
-if mode=='s' or mode=='d'
-
-  if ['22.11.3'].include? options.version
-    dataDir = energyDir + '/' + prod[:dataDir].call( options.radCor ? 'radcor' : 'noradcor' )
+elsif ['epic.22.11.2'].include? options.version
+  # print target directory
+  puts "Target Dir: #{prod[:targetDir]}"
+  # get list of Q2 subdirectories
+  q2dirList = mc_ls prod[:energyDir]
+  if q2dirList.empty?
+    $stderr.puts "ERROR: energy not found"
+    puts "Available energies"
+    system "mc ls #{prod[:releaseDir]}"
+    exit 1
+  end
+  puts "Q2 subdirectories: #{q2dirList}"
+  # get the Q2 ranges
+  prod[:q2ranges] = q2dirList.map do |dir|
+    [ dir.split('=').last.sub(/\/$/,'').to_i, 0 ]
+  end
+  puts "Q2 ranges: #{prod[:q2ranges]}"
+  # get a list of files for each Q2 range
+  puts "File names for each Q2 range:"
+  prod[:dataDirs] = []
+  prod[:fileLists] = prod[:q2ranges].map do |minQ2, maxQ2|
+    puts "--- #{minQ2} < Q2 < #{maxQ2}"
+    dataDir = prod[:energyDir] + '/' + prod[:dataSubDir].call(minQ2)
+    prod[:dataDirs] << dataDir
     puts "Data Dir: #{dataDir}"
-    # check if there are any files
-    if mc_ls("#{dataDir} | head").empty?
-      $stderr.puts "ERROR: no files in this Data Dir"
-      puts "Available Data Directory Tree (be patient...)"
-      system "mc tree #{releaseDir}"
-      exit 1
+    fileList = mc_ls(dataDir)
+      .first(options.limit)
+    puts "Files:"
+    fileList.each{ |file| puts "  #{file}" }
+    fileList
+  end
+  prod[:radDir] = '' # not used
+
+end # END RELEASE VERSION DEPENDENT CODDE ##################
+
+
+
+# download or stream the files, and build config file lists
+prod[:targetDir] += '/'+options.energy
+localFileTableName = "#{prod[:targetDir]}/files.config.list"
+localFileTable = File.open localFileTableName, 'w'
+prod[:q2ranges].zip(prod[:dataDirs],prod[:fileLists]).each do |q2range,dataDir,fileList|
+
+  # get the list of file names and set the target directory
+  minQ2, maxQ2 = q2range
+  targetDir = "#{prod[:targetDir]}/q2_#{minQ2}_#{maxQ2}"
+
+  # download the files
+  if options.mode=='d'
+    FileUtils.mkdir_p targetDir, verbose: true
+    fileList.each do |file|
+      system "mc cp '#{dataDir}/#{file}' #{targetDir}/"
+      puts ""
     end
-    # get the Q2 ranges
-    puts "Getting the full list of files... be patient..."
-    fullList = mc_ls(dataDir).grep(/\.root$/)
-    prod[:q2ranges] = fullList
-      .map{ |file| file.gsub(/.*_q2_/,'').sub(/_run.*/,'') }
-      .uniq
-      .map{ |range| range.split('_').map &:to_i }
-    puts "Q2 ranges: #{prod[:q2ranges]}"
-    # get a list of files for each Q2 range
-    puts "File names for each Q2 range:"
-    prod[:fileLists] = prod[:q2ranges].map do |minQ2, maxQ2|
-      fileList = fullList
-        .grep(/q2_#{minQ2}_#{maxQ2}/)
-        .first(options.limit)
-      puts "--- #{minQ2} < Q2 < #{maxQ2}"
-      fileList.each{ |file| puts "  #{file}" }
-      fileList
-    end
+  end
 
-  elsif ['22.11.2'].include? options.version
-    # get list of Q2 subdirectories
-    q2dirList = mc_ls energyDir
-    if q2dirList.empty?
-      $stderr.puts "ERROR: energy not found"
-      puts "Available energies"
-      system "mc ls #{releaseDir}"
-      exit 1
-    end
-    puts "Q2 subdirectories: #{q2dirList}"
-    # get the Q2 ranges
-    prod[:q2ranges] = q2dirList.map do |dir|
-      [ dir.split('=').last.sub(/\/$/,'').to_i, 0 ]
-    end
-    puts "Q2 ranges: #{prod[:q2ranges]}"
-    # get a list of files for each Q2 range
-    puts "File names for each Q2 range:"
-    prod[:fileLists] = prod[:q2ranges].map do |minQ2, maxQ2|
-      puts "--- #{minQ2} < Q2 < #{maxQ2}"
-      dataDir = energyDir + '/' + prod[:dataDir].call(minQ2)
-      puts "Data Dir: #{dataDir}"
-      fileList = mc_ls(dataDir)
-        .first(options.limit)
-      puts "Files:"
-      fileList.each{ |file| puts "  #{file}" }
-      fileList
-    end
+  # get the cross section
+  crossSection = getCrossSection.call( prod[:crossSectionID].call minQ2, maxQ2, prod[:radDir] )
 
-  end # if version
-end # if mode == 's' or 'd'
+  # build `localFileTable`
+  fileList.each do |fileBase|
+    file = options.mode=='s' ?
+      "#{dataDir.sub(/^S3/,'s3'+HostURL)}/#{fileBase}" : # if streaming, make S3 URL
+      "#{targetDir}/#{fileBase}"                         # otherwise, use local target path
+    localFileTable.puts "#{file} #{crossSection} #{minQ2} #{maxQ2}"
+  end
+
+end
+localFileTable.close
 
 
-# build full S3 file names
-prod[:fileLists].map!{ |file| "#{dataDir}/#{file}" }
-
-# download the files
-# prod[:q2range].zip(prod[:fileLists]).each do |q2range,fileList|
-#   mkdir "#{option.locDir}/#{option.energy}/q2_#{minQ2}_#{maxQ2}"
-#   fileList.each do |file ...
-# end
-
+# convert `localFileTable` into a full config file
+puts '.'*50
+puts "File Config Table: #{localFileTableName}"
+system "cat #{localFileTableName}"
+puts '\''*50
+configFile = options.configFile.empty? ?
+  localFileTableName.sub(/\.list/,'') :
+  options.configFile
+system "s3tools/generate-config-file.rb #{configFile} #{options.energy} #{localFileTableName}"
