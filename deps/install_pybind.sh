2f16d753c83695d2c5ba34de440c90a755fd9fef
Christopher Dilks <c-dilks@users.noreply.github.com>
2022-12-23 21:11:15 -0500

    build(HFSimpleTree): remove `pybind11` submodule (#222)

--- deps/install_pybind.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# update or download and install delphes
-
-### download/update source
-source environ.sh
-echo "[+] downloading pybind11 source"
-git submodule add -b stable ../../pybind/pybind11 deps/pybind11
-git submodule update --init
