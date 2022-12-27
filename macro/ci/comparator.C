1fafa2c7f51505e69538512d37d933e37a4c46b6
Christopher Dilks <christopher.j.dilks@gmail.com>
2022-12-27 14:19:08 -0500

    move `#define EXCLUDE_DELPHES` to the macro

--- macro/ci/comparator.C
+++ macro/ci/comparator.C
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: LGPL-3.0-or-later
 // Copyright (C) 2022 Christopher Dilks
 
+#define EXCLUDE_DELPHES
+
 R__LOAD_LIBRARY(EpicAnalysis)
 
 // make grids of plots, comparing data from the infiles
