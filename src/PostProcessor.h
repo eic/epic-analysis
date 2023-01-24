cc82865871922b84d1338d5ded12620f58d81022
Ralf-Christian Seidl <seidl@eic0108.rcf.bnl.gov>
2023-01-23 23:21:27 -0500

    Added method DrawRatioInBins which closely follows DrawInBins but plots the ratios to the first histogram given.

--- src/PostProcessor.h
+++ src/PostProcessor.h
@@ -91,6 +91,22 @@ class PostProcessor : public TNamed
         bool intgrid1=false, bool intgrid2=false,
         bool renormalize=false
         );
+    void DrawRatioInBins(
+        TString outName,
+        std::vector<std::vector<Histos*>>& histArr, TString histName,
+        TString var1name, int nvar1, double var1low, double var1high, bool var1log,
+        TString var2name, int nvar2, double var2low, double var2high, bool var2log,
+        bool intgrid1=false, bool intgrid2=false,
+        bool renormalize=false
+        );
+    void DrawRatioInBins(
+        TString outName,
+        std::vector<std::vector<std::vector<Histos*>>>& histArrList, TString histName,
+        TString var1name, int nvar1, double var1low, double var1high, bool var1log,
+        TString var2name, int nvar2, double var2low, double var2high, bool var2log,
+        bool intgrid1=false, bool intgrid2=false,
+        bool renormalize=false
+        );
 
     // algorithm finish methods; to be called after loops
     void FinishDumpAve(TString datFile);
