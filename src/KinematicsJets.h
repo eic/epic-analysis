c0b1559a69343f7391e202d4a575723e6dd579bd
Kevin Adkins <jkadkins@moreheadstate.edu>
2023-03-08 17:51:50 -0500

    New class for jet methods. Will soon be changed when implemented in the AnalysisEpic class.

---
+++ src/KinematicsJets.h
@@ -0,0 +1,78 @@
+// SPDX-License-Identifier: LGPL-3.0-or-later
+// Copyright (C) 2023 Kevin Adkins
+
+/* NOTE:
+ * if you make changes, MAINTAIN DOCUMENTATION IN ../doc/kinematicsJets.md
+ */
+
+#ifndef KinematicsJets_
+#define KinematicsJets_
+
+// Kinematics base calss
+#include "Kinematics.h"
+
+// Delphes includes
+#ifndef EXCLUDE_DELPHES
+#include "classes/DelphesClasses.h"
+#include "fastjet/ClusterSequence.hh"
+#ifdef INCLUDE_CENTAURO
+#include "fastjet/plugins/Centauro/Centauro.hh"
+#endif
+#endif
+
+using std::map;
+using std::cout;
+using std::cerr;
+using std::endl;
+
+class KinematicsJets : public Kinematics
+{
+  public:
+  KinematicsJets(Double_t enEleBeam, Double_t enIonBeam, Double_t crossAng);
+    ~KinematicsJets();
+
+    // DELPHES-specific methods ////////////////////////// -- to be removed when jets are added to AnalysisEpic
+#ifndef EXCLUDE_DELPHES
+
+    // jet calculators
+    void GetJets(
+        TObjArrayIter itEFlowTrack, TObjArrayIter itEFlowPhoton,
+        TObjArrayIter itEFlowNeutralHadron, TObjArrayIter itParticle,
+	int jetAlgo, double jetRadius, double jetMinPt
+        );
+    void CalculateJetKinematics(fastjet::PseudoJet jet);
+    void CalculateJetResolution(double deltaRCut);
+#ifdef INCLUDE_CENTAURO
+    void GetBreitFrameJets(
+        TObjArrayIter itEFlowTrack, TObjArrayIter itEFlowPhoton,
+        TObjArrayIter itEFlowNeutralHadron, TObjArrayIter itParticle
+        );
+    void CalculateBreitJetKinematics(fastjet::PseudoJet jet);
+#endif // ifdef INCLUDE_CENTAURO
+
+    // end DELPHES-specific methods //////////////////////////
+  
+    // jet objects
+    int jetAlgo;
+    double jetRad, jetMinPt;
+
+    std::vector<fastjet::PseudoJet> jetsRec, jetsTrue;
+    std::vector<fastjet::PseudoJet> breitJetsRec, breitJetsTrue;
+    std::map<double, int> jetConstituents;
+    fastjet::ClusterSequence csRec;
+    fastjet::ClusterSequence csTrue;
+
+    Double_t zjet, pTjet, qTjet, mTjet, etajet, phijet, mjet, ejet;
+    Double_t deltaRjet;
+    int matchStatusjet;
+    Double_t pTmtjet, mTmtjet, etamtjet, phimtjet, mmtjet, emtjet;
+    std::vector<double> jperp;
+    std::vector<double> zhad_jet;
+    // struck quark information
+    Double_t quarkpT;
+#endif // ifndef EXCLUDE_DELPHES
+
+  ClassDef(KinematicsJets,1);
+};
+
+#endif
