920282c6718f211d4528c559577f50f4e4a6dd90
Connor Pecar <39525107+cpecar@users.noreply.github.com>
2023-05-15 11:30:25 -0400

    Update src/Kinematics.h
    Co-authored-by: Christopher Dilks <c-dilks@users.noreply.github.com>

--- src/Kinematics.h
+++ src/Kinematics.h
@@ -1,4 +1,4 @@
- // SPDX-License-Identifier: LGPL-3.0-or-later
+// SPDX-License-Identifier: LGPL-3.0-or-later
 // Copyright (C) 2023 Christopher Dilks, Connor Pecar, Duane Byer, Sanghwa Park, Brian Page
 
 /* NOTE:
