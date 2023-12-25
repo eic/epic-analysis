1d208105d92bad9da5c80d1e89b3e18983e3e802
Christopher Dilks <c-dilks@users.noreply.github.com>
2023-12-25 02:25:35 -0500

    fix: stray comma

--- macro/ci/comparator_run.sh
+++ macro/ci/comparator_run.sh
@@ -33,7 +33,7 @@ done
 
 # run comparator.C
 args=""
-args+="\"$title1\",\"out/$name1.$pname.$recon.root\","
+args+="\"$title1\",\"out/$name1.$pname.$recon.root\"" #,"
 # args+="\"$title2\",\"out/$name2.$pname.$recon.root\","
 # args+="\"$title3\",\"out/$name3.$pname.$recon.root\","
 # args+="\"$title4\",\"out/$name4.$pname.$recon.root\","
