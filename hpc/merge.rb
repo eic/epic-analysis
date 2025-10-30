6a95da346aa5f76e65d202f24bab35fb187a7dca
Gregtom3 <gamatousek@gmail.com>
2025-10-30 15:24:13 -0400

    Enhance merge script: add output file removal if it exists; update input file listing in merge.rb; clean up whitespace in merge_analysis_files.cpp

--- hpc/merge.rb
+++ hpc/merge.rb
@@ -30,7 +30,13 @@ end
 outputDir      = ARGV[0]
 outputBaseName = ARGV[1] if ARGV.length>1
 PWD = `pwd`.strip
+puts ""
 # run merge_analysis_files.exe
 partFileList   = Dir.glob "#{outputDir}/parts/*.root"  # get list of input part files
 outputFileName = "#{outputDir}/#{outputBaseName}.root" # set output file name
+# Remove outputFileName if it already exists
+if File.exist? outputFileName
+  puts "Removing existing output file: #{outputFileName}"
+  File.delete outputFileName
+end
 system "#{PWD}/hpc/src/merge_analysis_files.exe #{outputFileName} #{partFileList.join ' '}" # merge
